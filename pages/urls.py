c204d81e079fc3ec1e09eead5b06972db670e3e3
ascadre <52655686+ascadre@users.noreply.github.com>
2019-08-01 10:07:50 +0200

    Add files via upload

--- pages/urls.py
+++ pages/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+
 urlpatterns = [
+    path('home/', views.index, name='index'),
     path('meteo/', views.meteo, name='meteo'),
     path('about/', views.vols, name='vols'),
-    path('', views.contact, name='contact'),
+    path('accounts/contact', views.contact, name='contact'),
     path('direct/', views.direct, name='direct'),
     path('sitac/',views.sitac,name='sitac'),
+    path('', auth_views.LoginView.as_view(template_name='pages/login.html')),
+    path('register/', views.register, name='register')
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 # Create your views here.
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login
+
 # Create your views here.
 def vols(request):
     context = {"vols_page": "active"} # new info here
@@ -13,15 +15,28 @@ def direct(request):
 def meteo(request):
     context = {"meteo_page": "active"} # new info here
     return render(request, 'pages/meteo.html', context)
+
 def contact(request):
-    if request.method == 'GET':
-        form = AuthentificationForm()
-        context = {"contact_page": "active"} # new info here
-        return render(request, 'pages/contact.html', context)
-    else:
-        form = AuthentificationForm(data=request.POST)
-        return render(request, "pages/login.html")
+    context = {"contact_page": "active"} # new info here
+    return render(request, 'registration/login.html', context)
 
 def sitac(request):
     context = {"sitac_page": "active"} # new info here
     return render(request, 'pages/sitac.html', context)
+
+def index(request):
+    context = {"index_page": "active"} # new info here
+    return render(request, 'pages/index.html', context)
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = authenticate(username = form.cleaned_data['username'], password = form.cleaned_data['password1'])
+            login(request, user)
+            return redirect('vols')
+    else :
+        form = UserCreationForm()
+    context = { "form" : form }
+    return render(request, 'pages/registration.html', context)
