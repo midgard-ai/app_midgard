4679b804f2bc215dd7246d94038a37f52243d24b
ascadre <52655686+ascadre@users.noreply.github.com>
2019-08-01 10:00:30 +0200

    add settings auth

--- main/urls.py
+++ main/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-#from django.contrib.auth.views import login
+from django.contrib.auth import views as auth_views
+from django.conf.urls import url
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
    # url(r'^login/$',login,{}),
     path('', include('pages.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
